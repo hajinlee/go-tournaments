e5f183e86b5dd2b1efd37026312e52a019e16339
Hajin Lee <hajin88@gmail.com>
2018-05-15 09:01:48 -0700

    Rspec tournament model first update

---
+++ spec/factories/tournaments.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :tournament do
+
+    title               "best go tournament ever"
+    venue               "go park"
+    start_date          2020-01-01
+    end_date            2020-01-02
+    city                "mountain view"
+    state               "CA"
+    organizer           "Haylee"
+    contact             "haylee@go.com"
+  end
+
+end
