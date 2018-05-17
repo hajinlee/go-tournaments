5ab36ac5e424945acb239dd107c2a12b4357f4fc
Hajin Lee <hajin88@gmail.com>
2018-05-17 10:09:48 -0700

    Table styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters_sign_up, :configure_permitted_parameters_update, if: :devise_controller?
+  before_action :configure_permitted_parameters_sign_up, 
+                :configure_permitted_parameters_update, if: :devise_controller?
 
   protected
 
@@ -10,5 +11,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters_update
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :aga_number])
   end
+
 
 end
