a8fbba66ab75401245bf0cb6b18414af021f2f27
Hajin Lee <hajin88@gmail.com>
2018-05-24 14:29:32 -0700

    more feature tests

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
 describe "registration path", type: :feature do
-  let(:password) {"abcdef"}
-  let(:user) {create :user, password: password}
+  let(:password) { "abcdef" }
+  let(:user) { create :user, password: password }
 
   before do
     visit new_user_session_path
@@ -10,29 +10,41 @@ describe "registration path", type: :feature do
     fill_in "Password", with: password
     click_button "Log in"
     expect(page).to have_content "Signed in successfully."
-    click_link "Create Tournament"
-    fill_in "Tournament Title:", with: "Bot Tournament"
-    fill_in "Start Date:", with: "11/11/2022"
-    fill_in "End Date:", with: "11/12/2022"
-    fill_in "Venue:", with: "go center"
-    fill_in "City:", with: "DC"
-    fill_in "State:", with: "DC"
-    fill_in "Organizer:", with: "test bot"
-    fill_in "Contact info:", with: "to the test"
-    click_button "Submit"
-    expect(page).to have_content "Bot Tournament"
   end
-  
+
   context "successful registration" do
     it "shows up on my page" do
-      visit "/"
-      click_link "Bot Tournament"
+      click_link "Create Tournament"
+      fill_in "Tournament Title:", with: "Bot Tournament II"
+      fill_in "Start Date:", with: "11/11/2022"
+      fill_in "End Date:", with: "11/12/2022"
+      fill_in "Venue:", with: "go center"
+      fill_in "City:", with: "DC"
+      fill_in "State:", with: "DC"
+      fill_in "Organizer:", with: "test bot"
+      fill_in "Contact info:", with: "to the test"
+      click_button "Submit"
+      expect(page).to have_content "Bot Tournament II"
+      click_link "Go Tournaments"
+      click_link "Bot Tournament II"
       click_button "Register"
       click_link "My Page"
       expect(page).to have_content "Unregister"
       click_link "Registrations"
-      expect(page).to have_content "Bot Tournament"
+      expect(page).to have_content "Bot Tournament II"
       expect(page).to have_content "1 players"
+    end
+  end
+
+  context "unregister link" do
+    it "deletes the registration" do
+      visit "/"
+      click_link "Bot Tournament II"
+      click_button "Register"
+      click_link "My Page"
+      expect(page).to have_content "Unregister"
+      click_link "Unregister"
+      expect(page).to have_no_content "Bot Tournament II"
     end
   end
 end
--- spec/features/tournament_page_spec.rb
+++ spec/features/tournament_page_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
 describe "create tournament path", type: :feature do
-  let(:password) {"abcdef"}
-  let(:user) {create :user, password: password}
+  let(:password) { "abcdef" }
+  let(:user) { create :user, password: password }
 
   before do
     visit new_user_session_path
@@ -26,6 +26,7 @@ describe "create tournament path", type: :feature do
       click_button "Submit"
       expect(page).to have_content "Bot Tournament"
       click_link "Go Tournaments"
+      expect(page).to have_content "Bot Tournament"
       click_link "All Tournaments List"
       expect(page).to have_content "Bot Tournament"
     end
@@ -44,4 +45,54 @@ describe "create tournament path", type: :feature do
       expect(page).to have_content "Organizer can't be blank"
     end
   end
+
+  context "tournament edit" do
+    it "saves a new info" do
+      click_link "Create Tournament"
+      fill_in "Tournament Title:", with: "Another Tournament"
+      fill_in "Start Date:", with: "11/11/2022"
+      fill_in "End Date:", with: "11/12/2022"
+      fill_in "Venue:", with: "go center"
+      fill_in "City:", with: "DC"
+      fill_in "State:", with: "DC"
+      fill_in "Organizer:", with: "test bot"
+      fill_in "Contact info:", with: "to the test"
+      click_button "Submit"
+      expect(page).to have_content "Another Tournament"
+      click_link "My Page"
+      expect(page).to have_content "Another Tournament"
+      click_link "Edit"
+      fill_in "Venue:", with: "Go Park"
+      click_button "Submit"
+      expect(page).to have_content "Go Park"
+    end
+  end
+
+=begin
+capybara can't accept the alert   
+  context "tournament delete" do
+    it "deletes the tournament" do
+      click_link "Create Tournament"
+      fill_in "Tournament Title:", with: "My Tournament"
+      fill_in "Start Date:", with: "11/11/2022"
+      fill_in "End Date:", with: "11/12/2022"
+      fill_in "Venue:", with: "go center"
+      fill_in "City:", with: "DC"
+      fill_in "State:", with: "DC"
+      fill_in "Organizer:", with: "test bot"
+      fill_in "Contact info:", with: "to the test"
+      click_button "Submit"
+      expect(page).to have_content "My Tournament"
+      click_link "My Page"
+      expect(page).to have_content "My Tournament"
+      click_link "Delete"
+      page.accept_alert "Are you sure?" do
+        click_button "OK"
+      end
+      click_link "Go Tournaments"
+      expect(page).to have_no_content "My Tournament"
+    end
+  end
+=end
+
 end
