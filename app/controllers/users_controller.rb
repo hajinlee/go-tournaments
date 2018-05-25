3a9b6aa264dd72467c1e1cd887152ece0b145a17
Hajin Lee <hajin88@gmail.com>
2018-05-24 19:31:44 -0700

    users_controller: use symbols for SQL identifiers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
-    @tournament = Tournament.where("author_user_id = ?", @user.id).order("start_date")
-    @registration = Registration.where("user_id = ?", @user.id)
+    @tournament = Tournament.where(author_user_id: @user.id).order(:start_date)
+    @registration = Registration.where(user_id: @user.id)
   end
 end
